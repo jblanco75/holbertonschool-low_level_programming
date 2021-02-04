15a423b4f705c82655b123425901c9cf19cd68fd
jblanco75 <jblanco75@gmail.com>
2021-02-04 01:25:41 +0000

    C hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-/* first attmpt */
+/**
+ * main - entry point
+ * Return: 0
+ */
 int main(void)
 {
+
 puts("Programming is like building a multilingual puzzle");
+
 return (0);
+
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
-#int < stdio.h >
-
+#include < stdio.h >
+/**
+ * main - entry point
+ * Return: 0
+ */
 int main(void)
 {
 
