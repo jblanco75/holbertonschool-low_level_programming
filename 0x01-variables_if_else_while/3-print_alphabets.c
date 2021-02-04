e2a97e1eeae4aeae094ec02c562b0fe59a220d27
jblanco75 <jblanco75@gmail.com>
2021-02-04 22:37:36 +0000

    for

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+char n;
+
+for (n = 'a'; n <= 'z' ; n++)
+putchar(n);
+for (n = 'A'; n <= 'Z' ; n++)
+putchar(n);
+putchar('\n');
+return (0);
+
+}
