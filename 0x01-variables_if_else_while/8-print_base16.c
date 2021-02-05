fd20a256d55f30aa1fa389af18bea8b92bd75946
jblanco75 <jblanco75@gmail.com>
2021-02-05 00:06:48 +0000

    for hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+char c;
+
+for (n = '0'; n <= '9' ; n++)
+putchar(n);
+for (c = 'a'; c <= 'f' ; c++)
+putchar (c);
+putchar('\n');
+return (0);
+}
