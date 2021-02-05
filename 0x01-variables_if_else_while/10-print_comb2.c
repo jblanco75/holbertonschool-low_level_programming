647926cfe7a966b43c068c08b17b8f8776e86ddd
jblanco75 <jblanco75@gmail.com>
2021-02-05 01:35:27 +0000

    loop

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -7,12 +7,11 @@ int main(void)
 {
 int n;
 
-
-for (n = '0'; n <= '100' ; n++)
+for (n = 0; n <= 100 ; n++)
 {
 putchar((n / 10) + '0');
 putchar((n % 10) + '0');
-if (n < '99')
+if (n != 99)
 {
 putchar(',');
 putchar(' ');
