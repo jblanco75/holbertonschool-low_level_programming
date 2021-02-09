0fc8334f6940c34f62b18120327c575172986ad5
jblanco75 <jblanco75@gmail.com>
2021-02-09 03:19:12 +0000

    functions_loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <holberton.h>
 /**
  * main - entry point
  * Return: 0
  */
 int main(void)
 {
-char n;
-
-printf("Holberton\n");
+int n;
+char out[n] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+for (n = 0; n <= 8 ; ++n)
+{
+_putchar(out[n]);
+}
+_putchar('\n');
 return (0);
 }
