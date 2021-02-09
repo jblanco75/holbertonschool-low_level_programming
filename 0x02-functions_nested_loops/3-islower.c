f28d5d96350e3b046e109d8896a0171492b83447
jblanco75 <jblanco75@gmail.com>
2021-02-09 19:53:15 +0000

    functions

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _islower : a bad copy of islower
+ * int c : variable to validate
+ * Return: 0 if not lowercase
+ */
+int _islower(int c);
+{
+int c;
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
