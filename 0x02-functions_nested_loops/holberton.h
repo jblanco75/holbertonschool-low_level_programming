c455cf19ff1cf26395cfb2cfdb8b43c20e1dfb76
jblanco75 <jblanco75@gmail.com>
2021-02-09 20:48:44 +0000

    functions_library

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,20 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/* func prototypes */
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+void print_no_zero(int n, int col_act, int limit);
+int add(int n, int m);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif /* _HOLBERTON_H_ */
