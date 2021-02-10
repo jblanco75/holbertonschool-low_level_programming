e71942a7eda4532008089f515720d0eed429e34d
jblanco75 <jblanco75@gmail.com>
2021-02-10 21:32:00 +0000

    functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,23 +7,37 @@
  */
 void print_to_98(int n)
 {
-if (n < 98)
-{
-for (; n <= 98 ; ++n)
-{
-printf("%d, ", n);
-}
-}
-else if (n > 98)
-{
-for (; n >= 98; --n)
-{
-printf("%d, ", n);
-}
-}
-else
-{
-printf("%d", n);
-}
-putchar('\n');
+	if (n < 98)
+	{
+		for (; n <= 98 ; ++n)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d", n);
+			}
+		}
+	}
+	else if (n > 98)
+	{
+		for (; n >= 98; --n)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d", n);
+			}
+		}
+	}
+	else
+	{
+		printf("%d", n);
+	}
+	printf("\n");
 }
