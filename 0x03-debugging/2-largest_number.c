bb9824bb40491b19c4e6dad14aeb6555631b58dd
jblanco75 <jblanco75@gmail.com>
2021-02-10 23:09:54 +0000

    debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,14 +14,17 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && a > c)
 	{
+		printf("%d is the largest number\n", a);
 		largest = a;
 	}
 	else if (b > a && b > c)
 	{
+		printf("%d is the largest number\n", b);
 		largest = b;
 	}
 	else
 	{
+		printf("%d is the largest number\n", c);
 		largest = c;
 	}
 
