5bd6db646b323bcbf02764b6ecaec87ba9a9c854
jblanco75 <jblanco75@gmail.com>
2021-02-10 20:14:14 +0000

    functions

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * jack_bauer - outputs last digit of a number
+ * Return: chrono
+ */
+void jack_bauer(void)
+{
+int x;
+int y;
+for (x = 0; x <= 23 ; x++)
+{
+for (y = 0; y <= 59; y++)
+{
+_putchar(x / 10 + '0');
+_putchar(x % 10 + '0');
+_putchar(':');
+_putchar(y / 10 + '0');
+_putchar(y % 10 + '0');
+_putchar('\n');
+}
+}
+}
