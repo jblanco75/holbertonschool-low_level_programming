7aea9a761ef311ab8be9a172b3f1ff7d58646fa2
jblanco75 <jblanco75@gmail.com>
2021-02-10 03:42:28 +0000

    functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - a bad copy of sum
+ * @a: variable to add
+ * @b: variable to add
+ * Return: result
+ */
+int add(int a, int b)
+{
+int result;
+result = a + b;
+return (result);
+}
