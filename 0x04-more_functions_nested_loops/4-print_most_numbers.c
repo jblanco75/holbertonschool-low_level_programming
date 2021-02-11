6af71226cf42668b61e010a24a4c44824be97e5b
jblanco75 <jblanco75@gmail.com>
2021-02-11 20:49:01 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - numbers 0-9 except 2,4
+ * Return: numbers
+ */
+void print_most_numbers(void);
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+		if (c == 2 || c == 4)
+		{
+			c++;
+			continue;
+		}
+		_putchar(c + '0');
+		c++;
+
+	}
+	_putchar('\n');
+}
