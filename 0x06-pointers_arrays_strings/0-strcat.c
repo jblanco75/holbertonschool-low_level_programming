d27a142bb6eab5c9fa516293d98ce828c2ccd233
jblanco75 <jblanco75@gmail.com>
2021-02-18 20:36:22 +0000

    arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,9 +7,11 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i = 0, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	while (dest[i] != '\0')
+		i++;
+
 	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i + j] = src[j];
