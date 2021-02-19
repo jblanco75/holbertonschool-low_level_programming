77918cb3df0a108097a6b71011fb38b8f374469e
jblanco75 <jblanco75@gmail.com>
2021-02-19 04:23:50 +0000

    arrays

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,28 @@
 #include "holberton.h"
 
 /**
- * leet - returns an array switched
- * @c: array
- * Return: array switched
+ * leet - Encodes a string into 1337.
+ *
+ * @s: String.
+ *
+ * Return: String in 1337.
  */
 
-char *leet(char *c)
+char *leet(char *s)
 {
 	int i, j;
 	char l[] = {'A', 'a', 'E', 'e', 'O', 'o', 'T', 't', 'L', 'l'};
 	char n[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
-	for (i = 0; c[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; l[j] != '\0'; j++)
 		{
-			if (c[i] == l[j])
+			if (s[i] == l[j])
 			{
-				c[i] = n[j];
+				s[i] = n[j];
 			}
 		}
 	}
-	return (c);
+	return (s);
 }
