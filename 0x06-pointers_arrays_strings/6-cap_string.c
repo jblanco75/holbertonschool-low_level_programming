fc64a8fe908765c835461608e41482aff7125ed8
jblanco75 <jblanco75@gmail.com>
2021-02-19 03:34:56 +0000

    arrays

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,12 +13,10 @@ char *cap_string(char *c)
 	for (i = 0; c[i] != '\0'; i++)
 	{
 		for (j = 0; z[j] != '\0'; j++)
-			if (c[i] == z[j])
+			if (c[i] == z[j] && c[i + 1] >= 'a' && c[i + 1] <= 'z')
 			{
-				i++;
-				if (c[i] >= 'a' && c[i] <= 'z')
-					c[i] = c[i] - 32;
+				c[i + 1] = c[i + 1] - 32;
 			}
-	}
+		}
 	return (c);
 }
