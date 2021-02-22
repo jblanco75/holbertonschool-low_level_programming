0af8fcabc0aa24f7ab4f5b924549d466a95422b4
jblanco75 <jblanco75@gmail.com>
2021-02-22 21:01:56 +0000

    more

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - copies a block of memory
+ *
+ * @dest: pointer
+ * @src: block to copy
+ * @n: number of spaces
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < 0; i++)
+	{
+	        dest[i] = src[i];
+	}
+	return (dest);
+}
