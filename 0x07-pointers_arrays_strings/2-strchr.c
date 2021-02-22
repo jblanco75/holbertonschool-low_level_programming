e7bc44a0b79205f7be2f6bafa24db783c9e3d086
jblanco75 <jblanco75@gmail.com>
2021-02-22 22:01:40 +0000

    more

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strchr - Returns a pointer to the first occurrence of the char
+ *
+ * @s: pointer
+ * @c: char to find
+ * Return: S or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		else
+		{
+			s++;
+		}
+	}
+	return (NULL);
+}
