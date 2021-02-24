8a9c2c8d264e8c63529ac97855e4a3f271304fea
jblanco75 <jblanco75@gmail.com>
2021-02-24 22:42:47 +0000

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - prints lentgh of a string recursively
+ * @s:string
+ * Return:string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
