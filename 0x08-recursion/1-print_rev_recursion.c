90534ce0575254f126bd8005ef5ae29c1f27cd86
jblanco75 <jblanco75@gmail.com>
2021-02-24 22:06:21 +0000

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - reverses a string recursively
+ * @s:string
+ * Return: reversed string
+ */
+void _print_rev_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
