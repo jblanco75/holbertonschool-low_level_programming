cb7d44ea7e558ef2290cc8041a6a7d3dce2b9791
jblanco75 <jblanco75@gmail.com>
2021-02-24 21:11:29 +0000

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,12 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
+		_putchar('\n');
 		return;
 	}
 	else
 	{
-		putchar(s[0]);
+		_putchar(s[0]);
 		_puts_recursion(&s[1]);
 	}
-	putchar('\n');
 }
