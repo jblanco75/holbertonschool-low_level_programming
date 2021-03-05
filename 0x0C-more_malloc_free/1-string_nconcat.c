2cee2fe6d3959210e59c0efeb9362e6149b186bc
jblanco75 <jblanco75@gmail.com>
2021-03-05 03:39:12 +0000

    more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,21 +1,5 @@
 #include "holberton.h"
 /**
- * _strlen - counts string chars
- * @s: variable to validate
- * Return: value
- */
-int _strlen(char *s)
-{
-	int c;
-
-	while (s[c] != '\0')
-	{
-		c++;
-	}
-	return (c);
-}
-
-/**
  * string_nconcat - concatenates two strings
  * @s1: variable to concat
  * @s2: variable ti concat
@@ -27,28 +11,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i = 0, j = 0;
 	char *s3;
-	unsigned int p;
-	unsigned int len_s1, len_s2;
 
 	if (s1 == 0)
 		s1 = "";
 	if (s2 == 0)
 		s2 = "";
 
-	len_s1 = _strlen(s1);
-	len_s2 = _strlen(s2);
+	while (s1[i])
+	{
+		i++;
+	}
+	while (s2[j])
+	{
+		j++;
+	}
+	if (n >= j)
+		n = j;
 
-	if (n >= len_s2)
-		n = len_s2;
-
-	p = len_s1 + n;
-
-	s3 = malloc(sizeof(char) * (p + 1));
+	s3 = malloc(sizeof(char) * (i + n + 1));
 
 	if (s3 == 0)
 		return (NULL);
 
-	for (i = 0; i < len_s1; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		s3[i] = s1[i];
 	}
