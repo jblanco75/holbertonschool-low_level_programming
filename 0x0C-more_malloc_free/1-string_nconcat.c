ddb3829daead3ad296c92608a9725969aeed2f99
jblanco75 <jblanco75@gmail.com>
2021-03-05 00:16:28 +0000

    more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,21 @@
 #include "holberton.h"
 /**
+ * _strlen - counts string chars
+ * @s: variable to validate
+ * Return: value
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
+
+/**
  * string_nconcat - concatenates two strings
  * @s1: variable to concat
  * @s2: variable ti concat
@@ -12,39 +28,34 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, j = 0;
 	char *s3;
 	int p;
-
-	p = n * 1;
+	unsigned int len_s1, len_s2;
 
 	if (s1 == 0)
 		s1 = "";
 	if (s2 == 0)
 		s2 = "";
 
-	while (s1[i])
-	{
-		i++;
-	}
-	while (s2[j])
-	{
-		j++;
-	}
+	len_s1 = _strlen(s1);
+	len_s2 = _strlen(s2);
 
-	i = i + j;
+	if (n >= len_s2)
+		n = len_s2;
 
-	s3 = malloc(sizeof(char) * (n + 1));
+	p = len_s1 + n;
+
+	s3 = malloc(sizeof(char) * p);
 
 	if (s3 == 0)
 		return (NULL);
-	if (p >= s2[j])
-		s3[i] = s2[j];
-	for (i = 0; s1[i]; i++)
+
+	for (i = 0; i < len_s1; i++)
 	{
 		s3[i] = s1[i];
 	}
-	for (j = 0; s2[j]; j++, i++)
+	for (j = 0; j < n; j++)
 	{
-		s3[i] = s2[j];
+		s3[len_s1 + j] = s2[j];
 	}
-	s3[i] = '\0';
+	s3[len_s1 + j] = '\0';
 	return (s3);
 }
