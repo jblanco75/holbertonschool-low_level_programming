ce37feb77daf73d98acc00991c3355ec5133840f
jblanco75 <jblanco75@gmail.com>
2021-03-09 03:38:36 +0000

    Structures_typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - frees variable blocks struct to define a dog
+ * @d: Struct variable
+ *
+ * Description: Structure var init
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL);
+	return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
