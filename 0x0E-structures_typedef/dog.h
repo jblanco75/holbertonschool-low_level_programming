37bbd7da4224e9941056d4de830ba16189d64930
jblanco75 <jblanco75@gmail.com>
2021-03-09 00:38:10 +0000

    Structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - Struct to define a dog
+ * @name: name of dog
+ * @owner: owner's name 
+ * @age: years old
+ *
+ * Description: Structure dog.
+ */
+typedef struct dog {
+
+	char *name;
+	char *owner;
+	float age;
+}dog;
+
+
+#endif
