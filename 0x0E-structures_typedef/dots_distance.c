4c01dd094eda609265b9a31fc33143533f4935e3
jblanco75 <jblanco75@gmail.com>
2021-03-10 01:12:23 +0000

    pld_challenge

---
+++ 0x0E-structures_typedef/dots_distance.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "point.h"
+/**
+ * new_point - defines new point
+ * @x: coord x axis
+ * @y: coord y axis
+ *
+ * Return: point struct
+ */
+
+struct Point *new_point(double *x, double *y)
+{
+	struct Point *point;
+
+	point = malloc(sizeof(struct Point));
+	if (point == NULL)
+		return (NULL);
+	point->x = x;
+	point->y = y;
+	return (point);
+}
+
+double calc_distance(struct Point *point1, struct Point *point2)
+{
+	double px, py;
+
+	px = (point1->x - point2->x);
+	py = (point1->y - point2->y);
+
+	return (sqrt((px * px) + (py * py)));
+}
+
+
+
+
+
+/**
+ * main - Returns distance between two points
+ * @argc: count
+ * @argv: arr
+ *
+ * Return: Always 0
+ **/
+
+int main(int argc, char **argv)
+{
+	int i;
+	double distance;
+	struct Point *point1;
+	struct Point *point2;
+
+	point1[] = (atof(argv[0]), atof(argv[1]));
+	point2[] = (atof(argv[2]), atof(argv[3]));
+       
+
+	if (argv[1] != '\0' && argv[2] != '\0')
+	{
+		for (i = 1; i < argc; i++)
+		{
+			distance = calc_distance();
+		}
+		printf("%f\n", distance);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
---
+++ 0x0E-structures_typedef/point.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct Point - Struct to define a point
+ * @x: coord x axis
+ * @y: coord y axis
+ *
+ * Description: Point
+ */
+struct Point
+{
+	double *x;
+	double *y;
+};
+
+#endif
