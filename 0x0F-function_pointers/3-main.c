d9972c451511777c465775e0897b08faa40fe10a
jblanco75 <jblanco75@gmail.com>
2021-03-11 03:59:08 +0000

    function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,29 +5,28 @@
  * @argv: arr
  * Return: Always 0
  **/
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int result;
-
 	if (argc != 4)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(98);
 	}
-
-	if (argv[2][1] != 0 || (*argv[2] != '/' && *argv[2] != '+' &&
-				*argv[2] != '*' && *argv[2] != '-' && *argv[2] != '%'))
+	if ((*argv[2] != '+' &&
+	     *argv[2] != '-' &&
+	     *argv[2] != '*' &&
+	     *argv[2] != '/' &&
+	     *argv[2] != '%') ||
+	    argv[2][1] != 0)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(99);
 	}
-	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	result = (get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
-	printf("%d\n", result);
-
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
