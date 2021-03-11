ba974781e7712e9dd32987c49ec0d6366310f86c
jblanco75 <jblanco75@gmail.com>
2021-03-11 00:35:39 +0000

    function pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - iterates elements of an array
+ * @array: array to iterate
+ * @size: size of array
+ * @cmp: function pointer
+ * Return: Nothing.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!cmp || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		(*cmp)(array[i]);
+		if ((*cmp)(array[i]) != 0)
+			return (i);
+	}
+}
