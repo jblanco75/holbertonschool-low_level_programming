7d5aa18d40f07a231153bccaf8a193adc23a059e
jblanco75 <jblanco75@gmail.com>
2021-03-11 00:07:52 +0000

    function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (!action || size <= 0)
+	if (!action || size <= 0 || !array || !size)
 		return;
 	for (i = 0; i < size; i++)
 	{
