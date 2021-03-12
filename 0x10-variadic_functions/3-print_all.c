fbfd5bb215f3c342c22b3d6fdac5c4622134970f
jblanco75 <jblanco75@gmail.com>
2021-03-12 02:46:30 +0000

    variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,82 +1,47 @@
 #include "variadic_functions.h"
 /**
- * p_char - print all the char args
- * @args: args passed to the function
- * Return: void function
- */
-void p_char(va_list args)
-{
-	printf("%c", va_arg(args, int));
-}
-/**
- * p_int - print all the int args
- * @args: args passed to the function
- * Return: void function
- */
-void p_int(va_list args)
-{
-	printf("%d", va_arg(args, int));
-}
-/**
- * p_float - print all the float args
- * @args: args passed to the function
- * Return: void function
- */
-void p_float(va_list args)
-{
-	printf("%f", va_arg(args, double));
-}
-/**
- * p_string - print all the string args
- * @args: args passed to the function
- * Return: void function
- */
-void p_string(va_list args)
-{
-	char *str;
-
-	str = va_arg(args, char *);
-	if (str == NULL)
-		printf("(nil");
-	else
-		printf("%s", str);
-}
-/**
  * print_all - prints all arguments according given format
  * @format: format to print
  * Return: void function
  */
-
 void print_all(const char * const format, ...)
 {
 	va_list row;
-	unsigned int i, j;
-	char *str = " ";
+	int i = 0;
+	char *str;
 
-	tp_s p_format[] = {
-		{"c", p_char},
-		{"i", p_int},
-		{"f", p_float},
-		{"s", p_string},
-		{NULL, NULL}
-
-	};
 	va_start(row, format);
-
-	i = 0;
-	while (format[i])
+	while (format == NULL)
 	{
-		j = 0;
-		while (p_format[j].tp != NULL)
+		printf("\n");
+		return;
+	}
+	while (format[i] != '\0')
+	{
+		switch (format[i])
 		{
-			if (format[i] == p_format[j].tp[0])
+		case 'c':
+			printf("%c", va_arg(row, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(row, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(row, double));
+			break;
+		case 's':
+			str = va_arg(row, char*);
+			if (str != NULL)
 			{
 				printf("%s", str);
-				p_format[j].f(row);
-				str = ", ";
+				break;
 			}
-			j++;
+			printf("(nil)");
+			break;
 		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
+		     || format[i] == 's') && format[i + 1] != '\0')
+			printf(", ");
 		i++;
 	}
 	printf("\n");
