7512fee3943f958474cc4088b6537811917ba349
jblanco75 <jblanco75@gmail.com>
2021-03-12 02:47:59 +0000

    variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,20 +10,5 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
-void p_char(va_list args);
-void p_int(va_list args);
-void p_float(va_list args);
-void p_string(va_list args);
-
-/**
- * struct tp_str - structure
- * @tp: char name for print
- * @f: function to print
- */
-typedef struct tp_str
-{
-	char *tp;
-	void (*f)(va_list);
-} tp_s;
 
 #endif
