8b596d2dbf5266b13a7fbede37b092958c603ff3
jblanco75 <jblanco75@gmail.com>
2021-03-19 21:16:22 +0000

    linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * print_list - Prints all the elements of a list_t list
+ * @h: Pointer to head of list
+ *
+ * Return: Nodes
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *s = h;
+	unsigned int i = 0;
+
+	if (s == NULL)
+		return (0);
+
+
+	while (s != NULL)
+	{
+		i++;
+		if (s->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] (%s)\n", s->len, s->str);
+		}
+		s = s->next;
+	}
+	return (i);
+}
