7815e44ca67231c850fc528ba3f1968099bc2dcc
jblanco75 <jblanco75@gmail.com>
2021-03-27 03:13:40 +0000

    bit manipulation

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * flip_bits - Returns the number of bits you would need
+ * to flip to get from one number to another.
+ * @n: Value decimal to decimal
+ * @m: Value decimal to decimal
+ *
+ * Return: values
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int count = 0;
+	int c = n ^ m;
+
+	while (c != 0)
+	{
+		c = c & (c - 1);
+		count++;
+	}
+	return (count);
+}
