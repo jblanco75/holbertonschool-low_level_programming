877a13e39a8401986598f1f5fb3042b61e0b31c9
jblanco75 <jblanco75@gmail.com>
2021-03-30 01:11:09 +0000

    file I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,6 +17,9 @@ int create_file(const char *filename, char *text_content)
 
 	of = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
+	if (of == EOF)
+		return (-1);
+
 	if (text_content != NULL)
 	{
 		while (text_content[i])
