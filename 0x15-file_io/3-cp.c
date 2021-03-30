35950e6d4e4a885b4f467d37230c0c80dabae390
jblanco75 <jblanco75@gmail.com>
2021-03-30 03:10:03 +0000

    file I/O

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+ * main - Program that copies the content of a file to another file
+ * @ac: count
+ * @av: array
+ *
+ * Return: Always 0
+ */
+int main(int ac, char *av[])
+{
+	int file_from, file_to, read_from, write_to;
+	char buffer[1024];
+
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+
+	file_from = open(av[1], O_RDONLY);
+
+	if (file_from < 0)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+	exit(98);
+
+	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (file_to < 0)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+
+	do {
+		read_from = read(file_from, buffer, 1024);
+		write_to = write(file_to, buffer, file_from);
+	} while (file_from == 1024);
+
+	if (read_from < 0)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	if (write_to < 0)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	if (close(file_from) < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+	if (close(file_to) < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+	return (0);
+}
