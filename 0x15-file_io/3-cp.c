62057d9c61acaf23c7c8fb334087df8554dc9e69
jblanco75 <jblanco75@gmail.com>
2021-03-30 03:18:29 +0000

    file I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,25 +12,25 @@ int main(int ac, char *av[])
 	char buffer[1024];
 
 	if (ac != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
+			exit(97);
 
 	file_from = open(av[1], O_RDONLY);
 
 	if (file_from < 0)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-	exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]),
+			exit(98);
 
 	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	if (file_to < 0)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]),
+			exit(99);
 
 	do {
 		read_from = read(file_from, buffer, 1024);
-		write_to = write(file_to, buffer, file_from);
-	} while (file_from == 1024);
+		write_to = write(file_to, buffer, read_from);
+	} while (read_from == 1024);
 
 	if (read_from < 0)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
