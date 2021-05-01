18bcdd2ccca011f912117480f54483ca64768861
jblanco75 <jblanco75@gmail.com>
2021-05-01 22:35:35 +0000

    Doubly lkd-lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,6 +14,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new = malloc(sizeof(dlistint_t));
 
+	if (new == NULL)
+		return (NULL);
+
 	new->prev = NULL;
 	new->n = n;
 	new->next = *head;
